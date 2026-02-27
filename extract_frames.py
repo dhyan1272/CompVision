6bdc6fe50b927e53c7bbe673638e80e9c99eaf5e
Nath <nathd@rpi.edu>
2026-02-27 17:01:01 -0500

    Fixed bug

--- extract_frames.py
+++ extract_frames.py
@@ -21,7 +21,7 @@ for split in os.listdir(dataset_folder):
  
         #Loop over clips/expressions of a user
         for clip_folder in os.listdir(user_path):
-            clip_path = os.path.join(user_path+"/"+clip_folder)
+            clip_path = os.path.join(user_path + "/" + clip_folder)
             if(not os.path.isdir(clip_path)):
                 continue
             print(clip_path)
@@ -30,23 +30,24 @@ for split in os.listdir(dataset_folder):
             for f in os.listdir(clip_path):
                 if f.lower().endswith(('.mp4', '.avi', '.mov', '.mkv')):
                     videos.append(f)
-                if not videos:
-                    continue
-                video_file = videos[0]
-                video_path = os.path.join(clip_path + "/" + video_file)
-                output_dir = os.path.join(output_root + "/" + clip_path)
-                os.makedirs(output_dir, exist_ok=True)
-                print(f"Extracting video from:{video_path}")
-                print(f"Output will be in:{output_dir}")
-                cap=cv2.VideoCapture(video_path)
-                frame_count=0
-                while True:
-                    ret, frame = cap.read()
-                    if not ret:
-                        break;
-                    # save image as jpeg
-                    frame_name = f"{os.path.splitext(video_file)[0]}_{frame_count+1:06d}.jpg"
-                    frame_path = os.path.join(output_dir + "/" + frame_name)
-                    cv2.imwrite(frame_path, frame)
-                    frame_count +=1
-                    print(frame_path) 
+            if not videos:
+                continue
+            video_file = videos[0]
+            video_path = os.path.join(clip_path + "/" + video_file)
+            rel_path = os.path.relpath(clip_path, dataset_folder)
+            output_dir = os.path.join(output_root, rel_path)
+            os.makedirs(output_dir, exist_ok=True)
+            print(f"Extracting video from:{video_path}")
+            print(f"Output will be in:{output_dir}")
+            cap=cv2.VideoCapture(video_path)
+            frame_count=0
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break;
+                # save image as jpeg
+                frame_name = f"{os.path.splitext(video_file)[0]}_{frame_count+1:06d}.jpg"
+                frame_path = os.path.join(output_dir + "/" + frame_name)
+                cv2.imwrite(frame_path, frame)
+                frame_count +=1
+                print(frame_path) 
